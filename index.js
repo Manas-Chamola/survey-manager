6260ad01aa0a6816881899b2315806c878bb8c26
Manas <chamolamanas19@gmail.com>
2020-07-01 22:58:30 +0530

    Adding billing and client side

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve production assets
+  app.use(express.static('client/build'));
+
+  //Serve index.js if it doesnt recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
