6d4599551d44dd16115db9ce0937beaa5d27f845
Manas <chamolamanas19@gmail.com>
2020-07-05 00:44:14 +0530

    Bugs Fixed

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Emaily!</h1>
       Collect feedback from users
+      <Link to="/surveys">
+        <h3>Go To Surveys</h3>
+      </Link>
     </div>
   );
 };
